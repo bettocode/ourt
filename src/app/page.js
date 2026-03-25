d7050eb90f079754795b670a248873b6ee8cfa69
bettocode <albejcc@gmail.com>
2026-03-25 22:05:12 +0100

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from 'next/link';
+
+
+export default function Home () {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <div>
+          <h1>Ourt</h1>
+          <p>Tu tiempo, tu dinero.</p>
+          <Link href="#">
+            <button>Comenzar</button>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
+}
